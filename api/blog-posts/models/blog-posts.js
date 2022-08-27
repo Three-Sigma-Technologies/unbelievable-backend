ded931a72a72e3ef8ee4801316fcd5d5910a3bd0
shonsirsha <seangeekpro@gmail.com>
2022-08-27 15:59:58 +0200

    added Reading time functionality for each blogpost

--- api/blog-posts/models/blog-posts.js
+++ api/blog-posts/models/blog-posts.js
@@ -1,10 +1,22 @@
 "use strict";
+const readingTime = require("reading-time");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
+//Returns in minute(s)
+const getReadingTime = (text) =>
+  Math.round(readingTime(text).minutes).toString();
+
 module.exports = {
-  lifecycles: {},
+  lifecycles: {
+    async afterUpdate(data) {
+      data.readTime = getReadingTime(data.content);
+    },
+    async afterCreate(data) {
+      data.readTime = getReadingTime(data.content);
+    },
+  },
 };
