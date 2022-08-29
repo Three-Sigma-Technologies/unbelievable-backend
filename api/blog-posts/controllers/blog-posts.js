229772160851a9a0bb52f9374b0f817564260820
shonsirsha <seangeekpro@gmail.com>
2022-08-29 06:21:48 +0200

    count views on get a new blog-post

--- api/blog-posts/controllers/blog-posts.js
+++ api/blog-posts/controllers/blog-posts.js
@@ -1,8 +1,29 @@
-'use strict';
+"use strict";
+
+const { sanitizeEntity } = require("strapi-utils/lib");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async findOne(ctx) {
+    const result = await strapi
+      .query("blog-posts")
+      .findOne({ slug: ctx.params.slug });
+
+    const newViews = result.views + 1;
+
+    await strapi
+      .query("blog-posts")
+      .update({ slug: ctx.params.slug }, { views: newViews });
+
+    return sanitizeEntity(
+      { ...result, views: newViews },
+      {
+        model: strapi.models["blog-posts"],
+      }
+    );
+  },
+};
