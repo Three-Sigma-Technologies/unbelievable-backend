fef8b69296327c1adc3f6f9f74bb5a524e2ba0cf
shonsirsha <seangeekpro@gmail.com>
2022-09-05 08:38:46 +0200

    created blogTopicsText for ease of search in BlogPost

--- api/blog-posts/models/blog-posts.js
+++ api/blog-posts/models/blog-posts.js
@@ -8,15 +8,35 @@ const readingTime = require("reading-time");
 
 //Returns in minute(s)
 const getReadingTime = (text) => Math.round(readingTime(text).minutes);
-
+const getBlogTopicsText = async (data) => {
+  const blogTopicsTextArr = await Promise.all(
+    data.blogTopics.map(
+      async (id) =>
+        (
+          await strapi.query("blog-topics").findOne({ id })
+        ).topicName
+    )
+  );
+  return blogTopicsTextArr.reduce((acc, curr, ix) => {
+    if (ix === 0) {
+      return acc.concat(curr);
+    } else {
+      return acc.concat(`, ${curr}`);
+    }
+  }, " ");
+};
 module.exports = {
   lifecycles: {
-    async afterUpdate(data) {
-      console.log(getReadingTime(data.content));
-      data.readTime = getReadingTime(data.content);
+    async beforeUpdate(_, data) {
+      if (Object.keys(data).length > 1) {
+        data.readTime = getReadingTime(data.content);
+        data.blogTopicsText = await getBlogTopicsText(data);
+      }
     },
-    async afterCreate(data) {
+
+    async beforeCreate(data) {
       data.readTime = getReadingTime(data.content);
+      data.blogTopicsText = await getBlogTopicsText(data);
     },
   },
 };
